d772d591c7fa8a5e8761fbbaaeab79c1465f7987
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-01-11 20:01:18 +0100

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
               <AnimatedText text="NoahDev0" />
             </h2>
 
-            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+            <h3 className="text-2xl py-2 text-secondary-foreground dark:text-white md:text-3xl">
               <AnimatedText duration={2.5} text=" Full Stack web developer" />
             </h3>
             <p className="text-md p-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
